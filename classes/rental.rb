0de5110f9301769365083a9bd202cc5cbe4ed464
ridwan <ridwanediallo99@gmail.com>
2022-08-26 21:19:01 +0000

    Update rental class

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Rental
   attr_accessor :date
   attr_reader :book, :person
@@ -5,20 +7,12 @@ class Rental
   def initialize(date, book, person)
     @date = date
 
+    @the_date = Date.parse(@date).strftime('%a %d %b %Y')
+
     @book = book
     @book.rentals << self
 
     @person = person
     @person.rentals << self
-  end
-
-  def person=(person)
-    @person = person
-    @person.rentals.push(self) unless @person.rentals.include?(self)
-  end
-
-  def book=(book)
-    @book = book
-    @book.rentals.push(self) unless @book.rentals.include?(self)
   end
 end
